c2e1c53b20c1e1754ad83784fde6e8327b2da3e9
Siddarth <110726331+siddarth2810@users.noreply.github.com>
2023-01-01 09:40:25 +0530

    changes have been made

--- index.js
+++ index.js
@@ -1,71 +1,71 @@
-// anychart.onDocumentReady(function () {
-//     anychart.data.loadJsonFile("https://static.anychart.com/git-storage/word-press/data/network-graph-tutorial/data_images.json", function (data) {
-//     // create chart from loaded data
-//     console.log(data)
-//     var chart = anychart.graph(data);
-//     // set title
-//     chart.title("Graph showing the choices");
-//     // access nodes
-//     var nodes = chart.nodes();
+let index=0;
+var quests= ['Quest 1', 'Quest 2 ','Quest 3']
+function newQuote() {
+document.getElementById("last").innerHTML=quests[index];
+index = index >= quests.length - 1 ? 0 : index + 1;
 
-//     // set the size of nodes
-//     nodes.normal().height(30);
-//     nodes.hovered().height(45);
-//     nodes.selected().height(45);
+}
 
-//     // set the stroke of nodes
-//     nodes.normal().stroke(null);
-//     nodes.hovered().stroke("#333333", 3);
-//     nodes.selected().stroke("#333333", 3);
-//     // enable labels of nodes
-//     chart.nodes().labels().enabled(true);
 
-//     // configure labels of nodes
-//     chart.nodes().labels().format("{%id}");
-//     chart.nodes().labels().fontSize(12);
-//     chart.nodes().labels().fontWeight(600);
-//     // draw chart
-//     chart.container("container").draw();
-//     });
-// });
 
-// create data
-anychart.onDocumentReady(function () {
 
-    // create data
-    var data = {
-      nodes: [
-        {id: "PATH"},
-        {id: "SAGE"},
-        {id: "KNIGHT"},
-        {id: "BARD"},
-        {id: "HEALER"},
-        {id: "SCOUT"},
-        
-      ],
-      edges: [
-        {from: "PATH", to: "SAGE"},
-        {from: "PATH", to: "KNIGHT"},
-        {from: "PATH", to: "BARD"},
-        {from: "PATH", to: "HEALER"},
-        {from: "PATH", to: "SCOUT"},
-      ]
+
+
+$(function() {
+    let treeview = {
+      resetBtnToggle: function() {
+        $(".js-treeview")
+          .find(".level-add")
+          .find("span")
+          .removeClass()
+          .addClass("fa fa-plus");
+        $(".js-treeview")
+          .find(".level-add")
+          .siblings()
+          .removeClass("in");
+      },
+      
+      addSubLevel: function(target) {
+        let liElm = target.closest("li");
+        let nextLevelCodeASCII = liElm.find("[data-level]").attr("data-level").charCodeAt(0) + 1;
+        liElm.children("ul").append($("#levelMarkup").html());
+        liElm.children("ul").find("[data-level]")
+          .attr("data-level", String.fromCharCode(nextLevelCodeASCII));
+      },
+     
     };
+  
+    // Treeview Functions
+    $(".js-treeview").on("click", ".level-add", function() {
+      $(this).find("span").toggleClass("fa-plus").toggleClass("fa-times text-danger");
+      $(this).siblings().toggleClass("in");
+    });
+  
+  
+  
+    // Add sub level
+    $(".js-treeview").on("click", "#knightclass"
+    , function() {
+      treeview.addSubLevel($(this));
+      // treeview.resetBtnToggle();
 
-    // create a chart and set the data
-    var chart = anychart.graph(data);
-
-    // set the chart title
-    chart.title("Network Graph: Basic Sample");
-
-    // set the container id
-    chart.container("container");
-
-    // initiate drawing the chart
-    chart.draw();
+      document.getElementById("last").innerHTML=quests[index];
+      index = index >= quests.length - 1 ? 0 : index + 1;
+      
+     
     
-});
+    });
 
-document.getElementById("container").addEventListener("click" ,function (){
-alert("will this work");
-});
+   
+    
+    $(".js-treeview").on("click", "#healerclass"
+    , function() {
+      treeview.addSubLevel($(this));
+      treeview.resetBtnToggle();
+      alert("you started healer quest")
+    });
+      
+  
+    
+  });
+  
